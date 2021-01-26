84b6e0ca1dabe412f48e5478804ba3ceec7b79fb
Prajakta Purohit <prajakta@chef.io>
2021-01-26 05:36:29 -0800

    Keep the es definition for now
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

---
+++ omnibus/config/software/elasticsearch.rb
@@ -0,0 +1,58 @@
+#
+# Copyright 2019 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "elasticsearch"
+default_version "6.8.12"
+
+dependency "server-open-jre"
+
+license "Apache-2.0"
+license_file "LICENSE.txt"
+skip_transitive_dependency_licensing true
+
+relative_path "elasticsearch-#{version}"
+
+version "6.8.12" do
+  source url: "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-#{version}.tar.gz",
+         sha512: "9f6179ee49baa48b49c5328b88ddf2f0ef868f49c1f04d77975622120749725c48ac09cd565c05f6033eb227eaff905aff9f881a85efcf2fa75cc586cb8c45cb"
+end
+
+version "7.9.1" do
+  source url: "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-#{version}-linux-x86_64.tar.gz",
+         sha512: "e24aab0fbeb0b53cc386bb0ca1fc84c457851c5d80d147324bf97ff42f063332a93dec3c693550662393a72c7a0522a100181dd9a7d50b3e487a0f2a2a9bbcc0"
+end
+
+target_path = "#{install_dir}/embedded/elasticsearch"
+
+build do
+  mkdir  "#{target_path}"
+  delete "#{project_dir}/lib/sigar/*solaris*"
+  delete "#{project_dir}/lib/sigar/*sparc*"
+  delete "#{project_dir}/lib/sigar/*freebsd*"
+  delete "#{project_dir}/config"
+  delete "#{project_dir}/modules/x-pack-ml"
+  delete "#{project_dir}/modules/ingest-geoip"
+  mkdir  "#{project_dir}/plugins"
+  # by default RPMs will not include empty directories in the final package
+  # ES will fail to start if this dir is not present.
+  touch  "#{project_dir}/plugins/.gitkeep"
+
+  sync   "#{project_dir}/", "#{target_path}"
+
+  # Dropping a VERSION file here allows additional software definitions
+  # to read it to determine ES plugin compatibility.
+  command "echo #{version} > #{target_path}/VERSION"
+end
